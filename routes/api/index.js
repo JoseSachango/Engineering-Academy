46844338b1c5731d1ea79953e90e4d1f022df037
JoseSachango <62193290+JoseSachango@users.noreply.github.com>
2021-03-11 19:20:25 -0600

    Axios post request for new project information was successful

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const unitTableRoutes = require("./unitTable");
 const streamRoutes = require("./Streams");
+const newProject = require("./newProject")
+const User = require("./User")
 // Book routes
 router.use("/unitTable", unitTableRoutes);
 router.use("/Streams", streamRoutes);
+router.use("/newProject",newProject)
+router.use("/User",User)
 
 module.exports = router;
